d39fa23bc7a409be4c2b37e50031bccb96d02754
JeffreyGrahamSanchezPerez <j.grahamsp@gmail.com>
2015-08-29 21:03:19 -0200

    Subido29/8/2015 7:02 PM

---
+++ V0.1/EcomenceWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EcomenceWeb.Global" Language="C#" %>
---
+++ V0.1/EcomenceWeb/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EcomenceWeb.Startup))]
+namespace EcomenceWeb
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
